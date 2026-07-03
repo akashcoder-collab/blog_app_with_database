edffe1162065f9699bbaef4943c0da8aef5add79
Akash Tiwari <anz026771@gmail.com>
2026-07-03 10:54:23 +0530

    Use Streamlit secrets

--- main.py
+++ main.py
@@ -3,19 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib
 import psycopg2
-import os
-from dotenv import load_dotenv
-
 matplotlib.use("Agg")
 
-load_dotenv()
 
 conn = psycopg2.connect(
-    host=os.environ["DB_HOST"],
-    database=os.environ["DB_NAME"],
-    user=os.environ["DB_USER"],
-    password=os.environ["DB_PASSWORD"],
-    port=os.environ["DB_PORT"]
+    host=st.secrets["DB_HOST"],
+    database=st.secrets["DB_NAME"],
+    user=st.secrets["DB_USER"],
+    password=st.secrets["DB_PASSWORD"],
+    port=st.secrets["DB_PORT"]
 )
 
 c = conn.cursor()
